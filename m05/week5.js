b30ef00d669c34aee906a12ab1ddfacf03dc2a10
mandy nishihara <mandynishihara@gmail.com>
2023-01-31 21:18:06 -0800

    com.

--- m05/week5.js
+++ m05/week5.js
@@ -2,11 +2,17 @@ var today = new Date();
 var hourNow = today.getHours();
 var greeting;
 
+var message = (firstname + " " + lastname);
+var firstname;
+var lastname;
 
 alert("Welcome User!");
+prompt("What is your first name", "first name");
+console.log(first name);
 
-var firstname = prompt("What is your first name");
-console.log(firstname);
+prompt("What is your last name", "last name");
+console.log(last name);
+
 
 
 if (hourNow > 18) {
