98ca951c32f68a19735715b09b4bd365ab8d1ffe
mandy nishihara <mandynishihara@gmail.com>
2023-02-01 02:13:25 -0800

    com

--- m05/m05moduleassignment.js
+++ m05/m05moduleassignment.js
@@ -1,15 +1,17 @@
 var table = Number(prompt('Enter a number'));             // Unit of table
-var operator = 'addition'; // Type of calculation
+var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
 var msg = '<h2>Multiplication Table</h2>';              // Message
 
-if (operator === 'addition') {
+
+if (operator === 'multiplication') {
   // Do addition
   while (i < 11) {
     msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
     i++;
   }
-} else {
+} else { 
+  
   // Do multiplication
   while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
