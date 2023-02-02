bfc889863174642e71353f51ad0de64874a9f389
mandy nishihara <mandynishihara@gmail.com>
2023-02-01 22:40:16 -0800

    com

--- m05/m05moduleassignment.js
+++ m05/m05moduleassignment.js
@@ -7,16 +7,17 @@ var msg = '<h2>Multiplication Table</h2>';              // Message
 if (operator === 'multiplication') {
   // Do addition
   while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} else { 
-  
-  // Do multiplication
-  while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
+  
+} else { 
+  while (i < 11) {
+    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+    i++;
+  }
+  // Do multiplication
+ 
 }
 
 // Write the message into the page
