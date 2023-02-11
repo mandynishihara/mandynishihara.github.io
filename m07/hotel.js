7f9566ef4f8043b9ece3d258a6a09b23b55699c0
mandy nishihara <mandynishihara@gmail.com>
2023-02-11 04:22:24 -0800

    com

--- m07/hotel.js
+++ m07/hotel.js
@@ -15,12 +15,12 @@ function Hotel(name, rooms, booked) {
   var sunsetHotel = new Hotel ('Sunset', 86,10);
   
   // Update the HTML for the page
-  var details1 = quayHotel.name + ' rooms: ';
+  var details1 = quayHotel.name + ' Rooms: ';
       details1 += quayHotel.checkAvailability();
   var elHotel1 = document.getElementById('hotel1');
   elHotel1.textContent = details1;
   
-  var details2 = parkHotel.name + ' rooms: ';
+  var details2 = parkHotel.name + ' Rooms: ';
       details2 += parkHotel.checkAvailability();
   var elHotel2 = document.getElementById('hotel2');
   elHotel2.textContent = details2;
@@ -30,7 +30,7 @@ function Hotel(name, rooms, booked) {
   You can use innerHTML on lines 21 and 26, but note the security issues on p228-231
   */
 
-  var details3 = sunsetHotel.name + ' rooms: ';
+  var details3 = sunsetHotel.name + ' Rooms: ';
   details3 += sunsetHotel.checkAvailability();
   var elHotel3 = document.getElementById('hotel3');
   elHotel3.textContent = details3;
